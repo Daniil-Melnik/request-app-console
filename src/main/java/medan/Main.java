1613d60ab4f3c2a2471c0985e8a59e973fd70d29
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-07-20 17:47:27 +0300

    -

--- src/main/java/medan/Main.java
+++ src/main/java/medan/Main.java
@@ -7,9 +7,9 @@ import medan.util.DataGenerator;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Start!");
-        //DataGenerator.generateEmployeesAndRequests(10, 1000);
-        RequestService service = new RequestService();
-        service.changeStatus(4, RequestStatus.NEW, 1);
+        DataGenerator.generateEmployeesAndRequests(1000, 1_000_000);
+        /*RequestService service = new RequestService();
+        service.changeStatus(4, RequestStatus.NEW, 1);*/
         System.out.printf("End!");
     }
 }
